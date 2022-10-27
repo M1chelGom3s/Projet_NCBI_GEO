9f28af00d7c97c7b4dfb9637e169d7fe17cd1efe
michel_gomes <michel.gomes@etu.umontpellier.fr>
2022-10-27 23:57:25 +0200

    change name file

--- app/app.r
+++ app/app.r
@@ -19,11 +19,13 @@ while (i < nb_row){
   
   gset = getGEO(accrssion, GSEMatrix =TRUE, getGPL=FALSE)
   for (gse in gset){
-    autor = gse@experimentData@name
-    title = gsub(" ", "_", gse@experimentData@title)
-    description = mac_table[i,4] 
+    autor =  gsub(",,",",",gse@experimentData@name)
+    title = gsub(".","-",gse@experimentData@title )
+    description = gse@experimentData@abstract
     acc = gse@experimentData@other[["geo_accession"]]
-    file_name = paste(autor, acc ,title, sep = "-")
+    overall_design = strsplit(gset[[1]]@experimentData@other[["overall_design"]], split = "[.] ")[[1]][[1]]
+    file_name = paste(overall_design, autor , sep = "-")
+    description = paste(title, autor,acc, description, sep = "\n")
     file_description = paste(file_name,"description.txt", sep = "/")
     file_dataCount= paste(file_name,"dataCount.tsv", sep = "/")
     file_Meta = paste(file_name,"metaData.tsv", sep = "/")
@@ -51,37 +53,3 @@ while (i < nb_row){
   }
   
 }
-
-# gse = getGEO(filename="dowloads/GSE113370_family.soft.gz",GSEMatrix=TRUE)
-# if (length(gse) > 1) idx = grep("GPL11180", attr(gse, "names")) else idx = 1
-# gse = gse[[1]]
-
-
-# ex = exprs(gse)
-# for (accrssion in acc$accessions){
-#   gset = getGEO(accrssion, GSEMatrix =TRUE, getGPL=FALSE)
-#   gset = gset[[1]]
-#   data_count = exprs(gset)
-#   meta = pData(gset)
-# }
-# gset = getGEO("GSE129178", GSEMatrix =TRUE, getGPL=FALSE)
-# gset = gset[[1]]
-# exprs(gset) %>% nrow()
-# gset = getGEOSuppFiles("GSE129178")
-
-# pData(gset)
-# Table(GSMList(gse)[[1]])[1:5,]
-# get the probeset ordering
-# probesets = Table(GPLList(gse)[[1]])$ID
-# make the data matrix from the VALUE columns from each GSM
-# being careful to match the order of the probesets in the platform
-# with those in the GSMs
-# data.matrix = do.call('cbind',lapply(GSMList(gse),function(x)
-# {tab = Table(x)
-# mymatch = match(probesets,tab$ID_REF)
-# return(tab$VALUE[mymatch])
-# }))
-# data.matrix = apply(data.matrix,2,function(x) {as.numeric(as.character(x))})
-# data.matrix = log2(data.matrix)
-# data.matrix[1:5,]
-# rm(gse)
