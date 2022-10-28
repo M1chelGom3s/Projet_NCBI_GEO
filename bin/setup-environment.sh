313b5e0d17b432f41a9b4e37152c4c7b7d713db4
michel_gomes <michel.gomes@etu.umontpellier.fr>
2022-10-28 20:15:06 +0200

    minor

--- bin/setup-environment.sh
+++ bin/setup-environment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker build -f bin/Dockerfile -t app_get_gep .
+sudo docker build -f bin/Dockerfile -t app_get_geo .
--- bin/start_python_app.sh
+++ bin/start_python_app.sh
@@ -1,2 +1,3 @@
-sudo docker run -d -t -i app_get_geo /bin/bash
+mkdir data
+sudo docker run -d -t -i -v $(pwd)/data:/project_GEO app_get_geo /bin/bash
 sudo docker exec -it $(sudo docker container ls  | grep 'app_get_geo' | awk '{print $1}') /bin/bash
